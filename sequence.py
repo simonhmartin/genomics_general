ba095dfda9480740c27a80283eeec6d1eb2e7d4a
Simon Martin <shm45@cam.ac.uk>
2017-09-01 15:57:34 +0100

    can now give start and end positions

--- sequence.py
+++ sequence.py
@@ -5,7 +5,7 @@
 
 import sys
 import argparse
-import DNA
+import genomics
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--phylipIn", help="Input is phylip format", action = "store_true")
@@ -17,6 +17,8 @@ parser.add_argument("-l", "--lineLen", help="Output line length", type = int, ac
 
 parser.add_argument("--truncateNames", help="Truncate names at first whitespace", action = "store_true")
 
+parser.add_argument("-s", "--start", help="Start position for subsection", type = int, action = "store", metavar = "integer")
+parser.add_argument("-e", "--end", help="End position for subsection", type = int, action = "store", metavar = "integer")
 
 args = parser.parse_args()
 #args = parser.parse_args("-n 5 -t test.trees -o test.topos.txt -w test.weights.B.csv -g A a,b,c -g B d,e,f -g C g,h,i -g D j,k,l".split())
@@ -29,22 +31,24 @@ else: outFormat = "fasta"
 
 l = args.lineLen
 
-if args.seqNames: outNames = args.seqNames.split(",")
 
 ########################################################################
 
 allText = sys.stdin.read()
 
-if inFormat == "fasta": names, seqs = DNA.parseFasta(allText)
-else: names, seqs = DNA.parsePhylip(allText)
+if inFormat == "fasta": names, seqs = genomics.parseFasta(allText)
+else: names, seqs = genomics.parsePhylip(allText)
 
 if args.truncateNames: names = [name.split()[0] for name in names]
 
-if outNames:
+if args.seqNames:
+    outNames = args.seqNames.split(",")
     keep = [names.index(name) for name in outNames]
     names = [names[k] for k in keep]
     seqs = [seqs[k] for k in keep]
-    
-sys.stdout.write(DNA.makeAlnString(names,seqs,outFormat,l))
+
+if args.start or args.end: seqs = [s[args.start-1:args.end] for s in seqs]
+
+sys.stdout.write(genomics.makeAlnString(names=names,seqs=seqs,outFormat=outFormat,lineLen=l))
 
 
