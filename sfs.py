69c947e8ff42f2c2fb0d55e16f081103f65b780b
Simon Martin <shm45@cam.ac.uk>
2018-07-01 11:38:52 +0100

    added option for individual based subsampling

--- sfs.py
+++ sfs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 '''Get frequency spectrum for each defined population, as well as the joint fs for all pairs and trips if requested'''
 
@@ -5,29 +6,6 @@ import sys,random,itertools,gzip,argparse
 from time import sleep
 import numpy as np
 import genomics
-
-
-
-ambigDict = {"K":["G","T"], "M":["A","C"], "R":["A","G"], "S":["C","G"], "W":["A","T"], "Y":["C","T"], "A":["A","A"], "C":["C","C"], "G":["G","G"], "T":["T","T"], "N":["N","N"] }
-
-def haplo(calls):
-    output = []
-    for call in calls:
-        if call in "ACGTNKRMSWY":
-            output += ambigDict[call]
-        else:
-            print "WARNING", call, "is not recognised as a valid base or ambiguous base"
-            output += ["N","N"]
-    return output
-
-def unique(things):
-    output = list(set(things))
-    output.sort()
-    return output
-
-def uniqueAlleles(bases):
-    output = unique([i for i in bases if i in "ACGT"])
-    return output
 
 
 def initializeFS(nHapDict,popNames = None, doPairs = False, doTrios = False, nIntervals = 1):
@@ -54,50 +32,6 @@ def initializeFS(nHapDict,popNames = None, doPairs = False, doTrios = False, nIn
             FS["_".join(p for p in trio)] = np.zeros([ncat[p] for p in trio] + [nIntervals])
     
     return FS
-
-
-def derCount(popBases, outBases):
-    outAllele = uniqueAlleles(outBases)
-    if len(outAllele) != 1:
-        return None
-    outAllele = outAllele[0]
-    
-    allPopBases = [b for pb in popBases for b in pb]
-    popAlleles = uniqueAlleles(allPopBases)
-    if len(popAlleles) > 2 or len(popAlleles) < 1:
-        return None
-    
-    if len(popAlleles) == 1 and popAlleles[0] == outAllele[0]:
-        return [0]*len(popBases)
-    
-    derAllele = [a for a in popAlleles if a != outAllele][0]
-    
-    derCounts = [p.count(derAllele) for p in popBases]
-    
-    return derCounts
-
-
-def jitter(numbers, maxDist = 0.001):
-  return [i + random.uniform(-maxDist, maxDist) for i in numbers]
-
-def minorCount(popBases):
-    allPopBases = [b for pb in popBases for b in pb]
-    popAlleles = uniqueAlleles(allPopBases)
-    
-    if len(popAlleles) > 2:
-        return None
-    
-    if len(popAlleles) == 1:
-        return [0]*len(popBases)
-    
-    #we count up and add a tint jitter to ensure that one allele is always lower, even if there is a tie
-    totalCounts = jitter([allPopBases.count(a) for a in popAlleles]) 
-    
-    minor = popAlleles[totalCounts.index(min(totalCounts))]
-        
-    minorCounts = [p.count(minor) for p in popBases]
-    
-    return minorCounts
 
 
 def fsTableString(array):
@@ -144,6 +78,8 @@ def whichInterval(scaffold, position, scafIntervals, intervalPosDict):
     return [i for i in scafInts if position in intervalPosDict[i]]
 
 
+def downSampleBaseCounts(baseCounts, N):
+    return np.bincount(np.random.choice(np.repeat(np.arange(4),baseCounts),N,replace=False),minlength=4)
 
 
 '#####################################################################################################################'
@@ -159,7 +95,10 @@ parser.add_argument("--ploidyFile", help="File with samples names and ploidy as 
 
 parser.add_argument("--doPairs", help="Do pairwise fs for all pairs.", action="store_true")
 parser.add_argument("--doTrios", help="Do trio fs for all trios.", action="store_true")
-parser.add_argument("-S", "--subSample", help="Subsample number for each population", action='store', required = False, metavar=("number of individuals"))
+parser.add_argument("--subSample", action='store', required = False,  nargs = "+", type=int,
+                    metavar=("number of bases"), help="Subsample bases number for each population")
+parser.add_argument("--subSampleIndividuals", action='store_true',
+                    help="Subsample by individual rather than treating bases separately")
 parser.add_argument("--pref", help="Prefix for output files", action = "store", required = False, default = "")
 parser.add_argument("--suff", help="Suffix for output files", action = "store", required = False, default = ".sfs")
 
@@ -169,9 +108,9 @@ parser.add_argument("--includeMono", help="Include counts at monomorphic sites (
 parser.add_argument("--dadiFormat", help="Format output sfs for dadi", action="store_true")
 
 #contigs
-parser.add_argument("--include", help="include contigs (separated by commas)", action='store')
+parser.add_argument("--include", help="include contigs", nargs = "+", action='store')
 parser.add_argument("--includeFile", help="File of contigs (one per line)", action='store')
-parser.add_argument("--exclude", help="exclude contigs (separated by commas)", action='store')
+parser.add_argument("--exclude", help="exclude contigs", nargs = "+", action='store')
 parser.add_argument("--excludeFile", help="File of contigs (one per line)", action='store')
 parser.add_argument("--intervalsFile", help="File of intervals, there will be a separate sfs for each", action='store')
 
@@ -181,7 +120,6 @@ parser.add_argument("--test", help="Verbose output", action="store_true")
 
 
 args = parser.parse_args()
-#args = parser.parse_args("-c /home/simon/software/egglib_test/test.geno -p mel ros.CJ531,ros.CJ533,ros.CJ546,ros.CJ2071 -p cyd cyd.CJ553,cyd.CJ560,cyd.CJ564,cyd.CJ565 -O hec.JM273 --doPairs --pref test".split())
 
 polarized = args.lastPopIsOutgroup
 if not polarized: sys.stderr.write("\nNo outgroup provided. Minor allele frequency will be used.\n")
@@ -191,12 +129,9 @@ doPairs = args.doPairs
 doTrios = args.doTrios
 
 subSample = args.subSample
-if subSample: ss=[int(x) for x in subSample.split(",")]
 
-include = []
-exclude = []
-if args.include: include += args.include.split(",")
-if args.exclude: exclude += args.exclude.split(",")
+include = args.include if args.include else []
+exclude = args.exclude if args.exclude else []
 
 if args.includeFile:
     with open(args.includeFile, 'r') as includeFile:
@@ -208,17 +143,17 @@ if args.excludeFile:
 
 if len(include) >= 1:
     include = set(include)
-    print >> sys.stderr, "\nIncluding", len(include), "contigs.\n"
+    sys.stderr.write("\nIncluding {} contigs.\n".format(len(include)))
 else: include = False
 
 if len(exclude) >= 1:
     exclude = set(exclude)
-    print >> sys.stderr, "\nExcluding", len(exclude), "contigs.\n"
+    sys.stderr.write("\nExcluding {} contigs.\n".format(len(exclude)))
 else: exclude = False
 
 intervalsFile = args.intervalsFile
 
-assert not (args.dadiFormat and intervalsFile), "You cannot specify separate intervale if outputting dadi format."
+assert not (args.dadiFormat and intervalsFile), "You cannot specify separate intervals if outputting dadi format."
 
 report = int(args.report)
 
@@ -238,7 +173,7 @@ header = line.split()
 
 #IF INTERVALS ARE SPECIFIED, STORE THESE
 if intervalsFile:
-    print >> sys.stderr, "Making interval index..."
+    sys.stderr.write("\nMaking interval index...\n")
     with open(intervalsFile, "rU") as intFile:
         intervalsList = [line.split() for line in intFile.readlines()]
     nIntervals = len(intervalsList)
@@ -281,12 +216,9 @@ allSamples = [s for popName in popDict for s in popDict[popName]]
 
 if polarized:
     inPopNames = popNames[:-1]
-    inPopDict = popDict.copy()
-    outgroupName = popNames.pop(-1)
-    OGsamples = popDict.pop(outgroupName) #haha don't get confused. Pop is also a function:)
+    outgroupName = popNames[-1]
 else:
     inPopNames = popNames
-    inPopDict = popDict
 
 sampleIndices = [header.index(s) for s in allSamples]
 
@@ -300,8 +232,8 @@ else: ploidyDict = dict(zip(allSamples,[2]*len(allSamples)))
 
 
 if args.verbose:
-    for popName in inPopNames: sys.stderr.write("\n"+ popName + ":\n" + ",".join(inPopDict[popName]) + "\n")
-    if polarized: sys.stderr.write("\noutgroup = "+ outgroupName + ":\n" + ",".join(OGsamples) + "\n")
+    for popName in inPopNames: sys.stderr.write("\n"+ popName + ":\n" + ",".join(popDict[popName]) + "\n")
+    if polarized: sys.stderr.write("\noutgroup = "+ outgroupName + ":\n" + ",".join(popDict[outgroupName]) + "\n")
 
 
 P = len(inPopNames)
@@ -323,12 +255,25 @@ nHapDict = dict([(popName, sum([ploidyDict[sample] for sample in popDict[popName
 
 #if sub sampling, first check that all pops have at least that number of samples
 if subSample is not None:
-    if len(subSample) == 1: ss = ss*len(inPopNames)
-    else: assert len(ss) == len(inPopNames), "subsample list must match number of ingroup populations"
-    ssDict = dict(zip(inPopNames, ss))
-    for p in inPopNames:
-        assert nHapDict[p] >= ssDict[p], "Population {} has fewer than {} haplotypes ({}).".format(p,ssDict[p],str(nHapDict[p])) 
-    nHapDict = ssDict
+    if not args.subSampleIndividuals:
+        if len(subSample) == 1: subSample = subSample*len(inPopNames)
+        else: assert len(subSample) == len(inPopNames), "subsample list must match number of ingroup populations"
+        ssDict = dict(zip(inPopNames, subSample))
+        for p in inPopNames:
+            assert nHapDict[p] >= ssDict[p], "Population {} has fewer than {} haplotypes ({}).".format(p,ssDict[p],str(nHapDict[p])) 
+        nHapDict = ssDict
+    else:
+        globalPloidy = np.unique(list(ploidyDict.values()))
+        assert(len(globalPloidy) == 1), "Subsampling by individuals not possible with variable ploidy"
+        if len(subSample) == 1: subSample = subSample*len(inPopNames)
+        else: assert len(subSample) == len(inPopNames), "subsample list must match number of ingroup populations"
+        ssDict = dict(zip(inPopNames, subSample))
+        _nHapDict_ = dict(zip(inPopNames, [s*globalPloidy[0] for s in subSample]))
+        for p in inPopNames:
+            assert nHapDict[p] >= _nHapDict_[p], "Population {} has fewer than {} haplotypes ({}).".format(p,_nHapDict_[p],str(nHapDict[p])) 
+        nHapDict = _nHapDict_
+
+nHapArray = [nHapDict[popName] for popName in inPopNames]
 
 #initialize arrays of allele counts
 arrays = initializeFS(nHapDict, inPopNames, doPairs, doTrios, nIntervals)
@@ -342,7 +287,7 @@ snpsCounted = 0
 for line in genoFile:
     
     linesDone += 1
-    if linesDone % report == 0: print >> sys.stderr, linesDone, "lines done..."
+    if linesDone % report == 0: sys.stderr.write("\n{} lines done...\n".format(linesDone))
     
     objects = line.split()
     
@@ -353,90 +298,75 @@ for line in genoFile:
     if intervalsFile: siteIntervals = whichInterval(objects[0], int(objects[1]), scafIntervals, intervalPosDict)
     else: siteIntervals = [0]
     
-    if siteIntervals:
-                
-        site = genomics.GenomeSite(genotypes=[objects[i] for i in sampleIndices], sampleNames=allSamples, popDict=popDict,
-                                ploidyDict=ploidyDict, genoFormat=args.genoFormat)
-            
-        popBases = dict([(popName, [b for b in site.asList(samples=popDict[popName], mode="bases") if b in "ACGT"],) for popName in inPopNames])
-        
-        if args.test:
-            sleep(0.25)
-            for popName in inPopNames:
-                sys.stderr.write("\n\n" + popName + ": " + " ".join(popBases[popName]))
-        
-        goodData = True
-        
-        #subsample if necessary
-        if subSample:
-            for popName in popNames:
-                try: popBases[popName] = random.sample(popBases[popName],ssDict[popName])
-                except: goodData = False
-            #if we get an error its because there's insufficient data in at least one pop, so we ignore the whole site
-            # in theory we could modify this part to use info for the pops it can - might be necessary when sites are limited
-        #if we didn't subsample, check that we have the right number of calls from each pop
-        else:
-            try: assert np.all([len(popBases[popName]) == nHapDict[popName] for popName in inPopNames])
-            except: goodData = False
-            # as in the subsample above, here we conservatively reject the entire line if one pop is missing data
-            # we might want to change this in future
-        
-        if not goodData: continue
-        
-        #allele counts per pop
-        countsArray = np.array([genomics.baseFreqs(popBases[popName], asCounts=True) for popName in inPopNames])
-        
-        #total allele counts summed over pops
-        inCounts = np.sum(countsArray, axis = 0)
-                
-        #check mono or biallelic
-        alleles = inCounts != 0
-        if not 1 <= np.sum(alleles) <= 2: continue
-        
-        #if we get here, the site has sufficient data to be considered for analysis
-        sitesAnalysed += 1
-        
-        # if there's an outgroup to polariize - we get the outgroup bases and then the derived counts, otherwise get minor allele counts
-        #the allele we count (minor or derived) will be called the target
-        if polarized:
-            outBases = [b for b in site.asList(samples=OGsamples, mode="bases") if b in "ACGT"]
-            
-            if args.test: sys.stderr.write("\n"+outgroupName + ": " + " ".join(outBases))
+    if not siteIntervals: continue
+    
+    site = genomics.GenomeSite(genotypes=[objects[i] for i in sampleIndices], sampleNames=allSamples, popDict=popDict,
+                            ploidyDict=ploidyDict, genoFormat=args.genoFormat)
+    
+    
+    popIndBaseCounts = dict([(popName, np.array([site.genotypes[indName].asBaseCounts() for indName in popDict[popName]]),) for popName in popNames])
+    
+    # get population basec counts, and do the subsampling if necessary
+    # This is currently conservative. If any one of the populations lacks sufficient good genotypes it will break
+    # in theory we could modify this part to use info for the pops it can - might be necessary when sites are limited
 
-            outCounts = genomics.baseFreqs(outBases, asCounts=True)
-            outAlleles = outCounts != 0
-            #check there is one outgroup allele and it is one of the ingroup alleles
-            if sum(outAlleles) != 1 or sum(outAlleles & alleles) > 2: continue
-            try: target = np.where(~outAlleles & alleles)[0][0]
-            except: target = np.where(~alleles)[0][0] #error here means invariant site, so take any absent allele as target 
+    if subSample:
+        if args.subSampleIndividuals:
+            popGoodInds = dict([(popName, np.where(popIndBaseCounts[popName].sum(axis=1) != 0)[0],) for popName in inPopNames])
+            try: popBaseCountsArray = np.array([popIndBaseCounts[popName][random.sample(popGoodInds[popName],ssDict[popName]),:].sum(axis = 0) for popName in inPopNames])
+            except: continue
+        else:
+            try: popBaseCountsArray = np.array([downSampleBaseCounts(popIndBaseCounts[popName].sum(axis = 0), nHapDict[popName]) for popName in inPopNames])
+            except: continue
+    else:
+        popBaseCountsArray = np.array([popIndBaseCounts[popName].sum(axis = 0) for popName in inPopNames])
+        if not np.all(popBaseCountsArray.sum(axis=1) == nHapArray): continue
+    
+    #total allele counts summed over pops
+    inBaseCounts = np.sum(popBaseCountsArray, axis = 0)
+    inAlleles = inBaseCounts > 0
+    
+    #check mono or biallelic
+    if not 1 <= inAlleles.sum() <= 2: continue
+    
+    if polarized:
+        outBaseCounts = popIndBaseCounts[outgroupName].sum(axis = 0)
+        outAlleles = outBaseCounts > 0
+        #check there is one outgroup allele and it is one of the ingroup alleles
+        if sum(outAlleles) != 1 or sum(outAlleles & inAlleles) > 2: continue
+        try: target = np.where(~outAlleles & inAlleles)[0][0]
+        except: target = np.where(~inAlleles)[0][0] #error here means invariant site, so take any absent allele as target 
+    
+    #otherwise take the minor (second most frequent) allele as target
+    else: target = inBaseCounts.argsort()[-2]
+    
+    #if we get here, the site has sufficient data to be considered for analysis
+    sitesAnalysed += 1
+    
+    if args.test: sys.stderr.write("\nTarget base: {} ".format(target))
+    
+    popTargetCounts = dict(zip(inPopNames, popBaseCountsArray[:,target]))
+    
+    if args.test:
+        for popName in inPopNames:
+            sys.stderr.write(popName + ": " + str(popTargetCounts[popName]) + ", ")
+    
+    #if we get here we are going to add this data to the SFS
+    snpsCounted += 1
+    
+    for si in siteIntervals:
+        for popName in inPopNames:
+            arrays[popName][popTargetCounts[popName],si] += 1
         
-        else: target = np.where(inCounts == sorted(inCounts)[-2])[0][0]
+        #for pairs
+        if doPairs:
+            for pairName in pairNames:
+                arrays[pairName][popTargetCounts[pairs[pairName][0]],popTargetCounts[pairs[pairName][1]], si] += 1
         
-        if args.test: sys.stderr.write("\nTarget base: {} ".format(target))
-        
-        popTargetCounts = dict(zip(inPopNames, countsArray[:,target]))
-        
-        if args.test:
-            for popName in inPopNames:
-                sys.stderr.write(popName + ": " + str(popTargetCounts[popName]) + " ")
-        
-        
-        #if we get here we are going to add this data tothe SFS
-        snpsCounted += 1
-        
-        for si in siteIntervals:
-            for popName in inPopNames:
-                arrays[popName][popTargetCounts[popName],si] += 1
-            
-            #for pairs
-            if doPairs:
-                for pairName in pairNames:
-                    arrays[pairName][popTargetCounts[pairs[pairName][0]],popTargetCounts[pairs[pairName][1]], si] += 1
-            
-            #check for trios
-            if doTrios:
-                for trioName in trioNames:
-                    arrays[trioName][popTargetCounts[trios[trioName][0]],popTargetCounts[trios[trioName][1]],popTargetCounts[trios[trioName][2]], si] += 1
+        #check for trios
+        if doTrios:
+            for trioName in trioNames:
+                arrays[trioName][popTargetCounts[trios[trioName][0]],popTargetCounts[trios[trioName][1]],popTargetCounts[trios[trioName][2]], si] += 1
 
 
 if not args.includeMono:
