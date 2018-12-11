e35fb465a1e1773cc67b44a5d95acad8dda93e87
Simon Martin <shm45@cam.ac.uk>
2018-12-11 22:47:19 +0000

    major rewrite to deal with multi-phylip files

--- seqToGeno.py
+++ seqToGeno.py
@@ -1,44 +1,8 @@
+#!/usr/bin/env python
+
 import gzip, argparse, sys
 
-
-def parseFasta(string):
-    splitString = string.split(">")[1:]
-    names = [s.split()[0] for s in splitString]
-    seqs = [s[s.index("\n"):].replace("\n","").replace(" ","") for s in splitString]
-    return (names,seqs)
-
-
-def parsePhylip(string):
-    cutString= string[string.index("\n"):]
-    names = [l.split()[0] for l in cutString.split("\n") if len(l.split()) == 2]
-    for name in names: cutString = cutString.replace("\n" + name + " ", "name")
-    splitString = cutString.split("name")[1:]
-    seqs = [s.replace("\n","").replace(" ","") for s in splitString]
-    return (names,seqs)
-
-def chunkList(l, nChunks = None, chunkSize = None, return_indices=False):
-    N = len(l)
-    assert not nChunks is chunkSize is None
-    if nChunks is not None:
-        assert N % nChunks == 0, "list must be divizable by number of chunks"
-        chunkSize = [N/nChunks]*nChunks
-    elif isinstance(chunkSize, int):
-        assert N % chunkSize == 0, "list must be divizable by chunk size"
-        chunkSize = [chunkSize]*(N/chunkSize)
-    else: assert N == sum(chunkSize), "Chunk sizes must sum to list length"
-        
-    indices = []
-    r = range(N)
-    i = 0
-    for c in chunkSize:
-        indices.append(range(i,i+c))
-        i = i+c
-    if return_indices: return ([[l[x] for x in ind] for ind in indices], indices,)
-    else: return [[l[x] for x in ind] for ind in indices]
-
-
-
-#############################
+import genomics
 
 parser = argparse.ArgumentParser()
 
@@ -49,7 +13,7 @@ parser.add_argument("-M", "--mode", help="Output mode for separate sequences", a
 parser.add_argument("-C", "--chrom", help="Name for chromosome or contig if sequences are samples", action = "store", required = False, default = "contig0")
 parser.add_argument("-N", "--name", help="Name for sample if sequences are contigs", action = "store", required = False, default = "sample0")
 parser.add_argument("-S", "--sequences", help="Sequences to output", action = "store", nargs="+", type=str)
-parser.add_argument("-P", "--ploidy", help="Ploidy for joining sequences", action = "store", nargs="+", type=int, default = 1)
+parser.add_argument("-P", "--ploidy", help="Ploidy for joining sequences", action = "store", nargs="+", type=int, default = [1])
 parser.add_argument("--randomPhase", help="Randomize phase for fused sequences", action = "store_true")
 
 args = parser.parse_args()
@@ -73,53 +37,66 @@ if args.randomPhase:
 seqString = seqFile.read()
 
 #parse
-if args.format == "fasta": seqNames,seqs = parseFasta(seqString)
-elif args.format == "phylip": seqNames,seqs = parsePhylip(seqString)
+if args.format == "fasta":
+    seqNames,seqs = genomics.parseFasta(seqString)
+    multi = False
 
-#sequences to keep
+elif args.format == "phylip":
+    #with phylip its possible to have multiple alignments, so we need to check if thats the case
+    pieces = genomics.parsePhylip(seqString)
+    if type(pieces) == tuple:
+        seqNames,seqs = pieces
+        multi = False
+    else:
+        _seqNames_,_seqs_ = zip(*pieces)
+        multi = True
 
-if args.sequences is not None:
-    seqs = [seqs[seqNames.index(x)] for x in args.sequences]
-    seqNames = args.sequences
 
+if not multi:
+    #if there is a single set of sequences we parse it and output either as contigs or individuals
+    #sequences to keep
+    if args.sequences is not None:
+        seqs = [seqs[seqNames.index(x)] for x in args.sequences]
+        seqNames = args.sequences
 
-#phase if ploidy is not 1
-if args.ploidy is not 1:
-    ploidy = args.ploidy
-    if len(ploidy) == 1:
-        assert len(seqNames) % ploidy[0] == 0, "Sequence number must be divizable by ploidy"
-        ploidy = ploidy*(len(seqNames)/ploidy[0])
-    else: assert len(seqNames) == sum(ploidy), "Ploidys must sum to number of sequences"
+    #phase if ploidy is not 1
+    if max(args.ploidy) > 1:
+        seqs, seqNames = genomics.haploToPhased(seqs, seqNames=seqNames, ploidy = args.ploidy)
+
+    ###output
     
-    indices = chunkList(range(len(seqNames)), chunkSize=ploidy[0], return_indices=True)[1]
+    if args.mode == "samples":
+        genoFile.write("#CHROM\tPOS\t" + "\t".join(seqNames) + "\n")
+        for x in xrange(len(seqs[0])): genoFile.write(args.chrom + "\t" + str(x+1) + "\t" + "\t".join([s[x] for s in seqs]) + "\n")
     
-    zipSeqs = [zip(*[seqs[x] for x in ind]) for ind in indices]
-    #randomize phase if necessary
-    if args.randomPhase:
-        for i in range(len(indices)):
-            if ploidy[i] > 1:
-                for j in range(len(zipSeqs[i])):
-                    zipSeqs[i][j] = random.sample(zipSeqs[i][j], ploidy[i])
-    seqs = [["|".join(x) for x in zipSeq] for zipSeq in zipSeqs]
-    seqNames = ["_".join([seqNames[x] for x in ind]) for ind in indices]
+    elif args.mode == "contigs":
+        genoFile.write("#CHROM\tPOS\t" + args.name + "\n")
+        for y in range(len(seqNames)):
+            for x in xrange(len(seqs[y])): genoFile.write(seqNames[y] + "\t" + str(x+1) + "\t" + seqs[y][x] + "\n")
 
-
-#############################
-
-
-if args.mode == "samples":
+else:
+    #otherwise we output each set as a contig, with each sequence as a sample
+    #first confirm all same length
+    assert len(set(map(len,_seqNames_))) == 1, "For multi phylip, all alignments must have same number of sequences"
+    #sequences to keep
+    seqNames = args.sequences if args.sequences else _seqNames_[0]
+    #now we need to stich together all sequences, but they are possibly different orders, so index each one each time
+    _indices_ = [[_names_.index(name) for name in seqNames] for _names_ in _seqNames_]
+    _seqs_ = [[_seqs_[i][j] for j in _indices_[i]] for i in range(len(_seqs_))]
+    
+    #phase if ploidy is not 1
+    if max(args.ploidy) > 1:
+        _seqs_ = [genomics.haploToPhased(seqs, ploidy = args.ploidy) for seqs in _seqs_]
+        seqNames = genomics.makePhasedNames(seqNames, ploidy = args.ploidy)
+    
+    ###output
+    #with multi data there is only one output mode, so we ignore the mode specified.
     genoFile.write("#CHROM\tPOS\t" + "\t".join(seqNames) + "\n")
-    for x in xrange(len(seqs[0])): genoFile.write(args.chrom + "\t" + str(x+1) + "\t" + "\t".join([s[x] for s in seqs]) + "\n")
-
-elif args.mode == "contigs":
-    genoFile.write("#CHROM\tPOS\t" + args.name + "\n")
-    for y in range(len(seqNames)):
-        for x in xrange(len(seqs[y])): genoFile.write(seqNames[y] + "\t" + str(x+1) + "\t" + seqs[y][x] + "\n")
-    
+    for i,seqs in enumerate(_seqs_):
+        for x in xrange(len(seqs[0])): genoFile.write(args.chrom + str(i) + "\t" + str(x+1) + "\t" + "\t".join([s[x] for s in seqs]) + "\n")
 
 #############################
 
 
 seqFile.close()
 genoFile.close()
-
