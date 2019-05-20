MAJOR bug fix for individual distance matrix computation as used by distMat.py

genomics.py
@@ -890,13 +890,13 @@ class Alignment:
             pairDists = {}
             for sampleName in sampleNames: pairDists[sampleName] = {} 
             for i,j in itertools.product(range(n),repeat=2):
-                pairDists[sampleNames[i]][sampleNames[j]] = np.mean(distMat[sampleIndices[i],sampleIndices[j]])
+                pairDists[sampleNames[i]][sampleNames[j]] = np.mean(distMat[np.ix_(sampleIndices[i],sampleIndices[j])])
             
             return pairDists
         else:
             indDistMat = np.zeros(n,n)
             for i,j in itertools.combinations_with_replacement(range(n),2):
-                    indDistMat[i,j] = indDistMat[j,i] = np.mean(distMat[sampleIndices[i],sampleIndices[j]])
+                    indDistMat[i,j] = indDistMat[j,i] = np.mean(distMat[np.ix_(sampleIndices[i],sampleIndices[j])])
             return indDistMat
     
     def groupDistStats(self, doPairs = True):